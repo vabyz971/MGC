d81fba0193ee00f01928d015e7b11d685a15e525
Vabyz971 <vabyz971@gmail.com>
2020-10-21 18:29:57 -0400

    affiche le nb d'images

--- project/dashboard/views.py
+++ project/dashboard/views.py
@@ -1,9 +1,15 @@
 from django.utils.translation import ugettext_lazy as _
 from nierInterface.generic import TemplateView
 
-# Create your views here.
+from pictures.models import Picture
 
 
 class DashboardHomeView(TemplateView):
     title = _('Dashboard')
     template_name = 'dashboard/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        images = Picture.objects.all()
+        ctx['images_list'] = images.count()
+        return ctx
